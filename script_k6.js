baf2b5f47ac37556deba7292815dad2177ce5288
JairHJ-dev <2022171044@uteq.edu.mx>
2026-03-02 14:09:17 -0600

    Update script_k6.js

--- script_k6.js
+++ script_k6.js
@@ -3,20 +3,22 @@ import { sleep, check } from 'k6';
 
 export const options = {
   stages: [
-    { target: 10, duration: '30s' },
-    { target: 10, duration: '1m' },
-    { target: 0, duration: '30s' },
+    { target: 10, duration: '30s' }, // Sube a 10 usuarios
+    { target: 10, duration: '1m' },  // Mantiene 10 usuarios
+    { target: 0, duration: '30s' },  // Baja a 0
   ],
 };
 
 export default function () {
-  // Vista 1: Home
+
   let res1 = http.get('http://localhost:3000/'); 
-  check(res1, { 'Home OK': (r) => r.status === 200 });
+  check(res1, { 'Home carga OK': (r) => r.status === 200 });
+
   sleep(1);
 
-  // Vista 2: API (ajusta el endpoint si tienes uno diferente)
+
   let res2 = http.get('http://localhost:3000/api/status'); 
-  check(res2, { 'API OK': (r) => r.status === 200 });
+  check(res2, { 'API responde OK': (r) => r.status === 200 });
+
   sleep(1);
 }
