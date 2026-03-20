051b7febede490d79bc205239c443a07c90df33c
JairHJ-dev <2022171044@uteq.edu.mx>
2026-03-20 13:32:28 -0600

    Agregando métricas de Prometheus a la app

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+// --- INICIO CÓDIGO PROMETHEUS ---
+const client = require('prom-client');
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var itemsRouter = require("./routes/items");
+// 1. Habilitar métricas por defecto de Node.js
+client.collectDefaultMetrics();
 
-var app = express();
+// 2. Crear métricas personalizadas
+const httpRequestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total de peticiones HTTP procesadas',
+  labelNames: ['metodo', 'ruta', 'estado_http'],
+});
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+const activeUsersGauge = new client.Gauge({
+  name: 'active_users_current',
+  help: 'Número actual de usuarios activos simulados'
+});
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/items", itemsRouter);
+// Middleware para contar todas las peticiones
+app.use((req, res, next) => {
+  res.on('finish', () => {
+    httpRequestCounter.labels(req.method, req.route ? req.route.path : req.path, res.statusCode).inc();
+  });
+  next();
+});
 
-module.exports = app;
+// 3. RUTA VITAL: Endpoint /metrics
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.send(await client.register.metrics());
+});
+// --- FIN CÓDIGO PROMETHEUS ---
